10e7a0e1ca168db4adc7f22f8e9b457fa99e90e0
Mosiur Rahman Sweet <sweet.csevu@gmail.com>
2024-11-23 18:25:13 +0600

    Flutter Class #2

--- lib/main.dart
+++ lib/main.dart
@@ -10,36 +10,116 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(width: double.infinity),
+              const Text(
+                "Mosiur Rahman Sweet",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                "Flutter Developer",
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 18,
+                ),
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.location_on,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    "Rajshahi",
+                  ),
+                ],
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.phone),
+                  Text("+8801521473707"),
+                  SizedBox(width: 10),
+                  Icon(Icons.email),
+                  Text("sweet.csevu@gmail.com"),
+                ],
+              ),
+              const Divider(),
+              const Text(
+                "PROFESSIONAL STATEMENT",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3,
+                  wordSpacing: 5,
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text(
+                'I am a dedicated Flutter developer with hands-on experience in designing and delivering robust, high-performing mobile applications. My expertise lies in creating user-centric solutions that prioritize efficiency, seamless functionality, and exceptional user experiences. I am seeking a challenging role where I can leverage my skills to contribute innovative solutions and elevate Flutter development standards.',
+                textAlign: TextAlign.justify,
+              ),
+              const Divider(),
+              const Text(
+                "SKILLS",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3,
+                  wordSpacing: 5,
+                ),
+              ),
+              const SizedBox(height: 5),
+              Wrap(
+                alignment: WrapAlignment.center,
+                runSpacing: 10,
+                children: [
+                  skill("flutter"),
+                  const SizedBox(width: 10),
+                  skill("java"),
+                  const SizedBox(width: 10),
+                  skill("firebase"),
+                  const SizedBox(width: 10),
+                  skill("supabase"),
+                  const SizedBox(width: 10),
+                  skill("state management"),
+                  const SizedBox(width: 10),
+                  skill("clean architecture"),
+                ],
+              ),
+              const Divider(),
+              const Spacer(),
+              const Text("© Orange Owl - 2024"),
+              const SizedBox(height: 10),
+            ],
+          ),
+        )),
       ),
-      home: const MyHomePage(),
     );
   }
-}
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const SizedBox(width: 1000),
-        const SizedBox(height: 50),
-        Text(
-          'Mosiur Rahman Sweet',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blueGrey[800]),
-        ),
-        const SizedBox(height: 5),
-        const Text(
-          "I'm a Flutter Developer",
-        )
-      ],
-    ));
+  Container skill(String name) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 8),
+      decoration: BoxDecoration(
+        border: Border.all(),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        name,
+        style: const TextStyle(fontSize: 18),
+      ),
+    );
   }
 }
