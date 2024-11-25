2798290e5268f042b89302013cd63f1a6da88822
Mosiur Rahman Sweet <sweet.csevu@gmail.com>
2024-11-25 17:19:22 +0600

    assignment #2

--- lib/main.dart
+++ lib/main.dart
@@ -6,105 +6,168 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.grey.shade200,
         body: SafeArea(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(width: double.infinity),
-              const Text(
-                "Mosiur Rahman Sweet",
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Text(
-                "Flutter Developer",
-                style: TextStyle(
-                  fontStyle: FontStyle.italic,
-                  fontSize: 18,
-                ),
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.location_on,
-                    color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(width: double.infinity),
+                const SizedBox(height: 5),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(),
                   ),
-                  Text(
-                    "Rajshahi",
+                  child: const Column(
+                    children: [
+                      Text(
+                        "Mosiur Rahman Sweet",
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Flutter Developer",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.black,
+                          ),
+                          Text(
+                            "Rajshahi, Bangladesh",
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.phone),
+                          SizedBox(width: 3),
+                          Text("+8801521473707"),
+                          SizedBox(width: 10),
+                          Icon(Icons.email),
+                          SizedBox(width: 3),
+                          Text("sweet.csevu@gmail.com"),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.phone),
-                  Text("+8801521473707"),
-                  SizedBox(width: 10),
-                  Icon(Icons.email),
-                  Text("sweet.csevu@gmail.com"),
-                ],
-              ),
-              const Divider(),
-              const Text(
-                "PROFESSIONAL STATEMENT",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 3,
-                  wordSpacing: 5,
                 ),
-              ),
-              const SizedBox(height: 5),
-              const Text(
-                'I am a dedicated Flutter developer with hands-on experience in designing and delivering robust, high-performing mobile applications. My expertise lies in creating user-centric solutions that prioritize efficiency, seamless functionality, and exceptional user experiences. I am seeking a challenging role where I can leverage my skills to contribute innovative solutions and elevate Flutter development standards.',
-                textAlign: TextAlign.justify,
-              ),
-              const Divider(),
-              const Text(
-                "SKILLS",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 3,
-                  wordSpacing: 5,
+                const SizedBox(height: 15),
+                sectionHeading("PROFESSIONAL STATEMENT"),
+                const SizedBox(height: 10),
+                const Text(
+                  'I am a dedicated Flutter developer with hands-on experience in designing and delivering robust, high-performing mobile applications. My expertise lies in creating user-centric solutions that prioritize efficiency, seamless functionality, and exceptional user experiences.',
+                  textAlign: TextAlign.justify,
                 ),
-              ),
-              const SizedBox(height: 5),
-              Wrap(
-                alignment: WrapAlignment.center,
-                runSpacing: 10,
-                children: [
-                  skill("flutter"),
-                  const SizedBox(width: 10),
-                  skill("java"),
-                  const SizedBox(width: 10),
-                  skill("firebase"),
-                  const SizedBox(width: 10),
-                  skill("supabase"),
-                  const SizedBox(width: 10),
-                  skill("state management"),
-                  const SizedBox(width: 10),
-                  skill("clean architecture"),
-                ],
-              ),
-              const Divider(),
-              const Spacer(),
-              const Text("© Orange Owl - 2024"),
-              const SizedBox(height: 10),
-            ],
+                const SizedBox(height: 15),
+                sectionHeading("SKILLS"),
+                const SizedBox(height: 10),
+                Wrap(
+                  spacing: 10, //* horizontal spacing
+                  runSpacing: 10, //* vertical spacing
+                  alignment: WrapAlignment.spaceBetween,
+                  children: [
+                    skill("flutter"),
+                    skill("java"),
+                    skill("firebase"),
+                    skill("supabase"),
+                    skill("state management"),
+                    skill("clean architecture"),
+                  ],
+                ),
+                const SizedBox(height: 15),
+                sectionHeading("EDUCATION"),
+                const SizedBox(height: 10),
+                education(
+                  "BSc in CSE",
+                  2020,
+                  "Varendra University",
+                  "GPA * out of 4",
+                  CrossAxisAlignment.start,
+                ),
+                education(
+                  "HSC (Science)",
+                  2016,
+                  "XYZ College",
+                  "GPA * out of 5",
+                  CrossAxisAlignment.end,
+                ),
+                education(
+                  "SSC (Science)",
+                  2014,
+                  "Varendra University",
+                  "GPA * out of 4",
+                  CrossAxisAlignment.start,
+                ),
+                const Divider(),
+                const Spacer(),
+                const Text("© Orange Owl 2024"),
+                const SizedBox(height: 5),
+              ],
+            ),
           ),
-        )),
+        ),
+      ),
+    );
+  }
+
+  SizedBox education(String degree, int year, String institute, String grade, CrossAxisAlignment alignment) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: alignment,
+        children: [
+          Text(
+            '$degree | $year',
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            institute,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            grade,
+            style: const TextStyle(
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container sectionHeading(String text) {
+    return Container(
+      color: Colors.black,
+      width: double.infinity,
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+        ),
       ),
     );
   }
